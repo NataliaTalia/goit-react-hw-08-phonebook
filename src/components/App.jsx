51044ec419a4a9f75c5b264285b0224045ebfb03
Natalia <nataliamaliovana.nm@gmail.com>
2023-10-16 18:15:08 +0700

    last tweaks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,17 @@ import Home from 'pages/Home';
 import Register from 'pages/Register';
 import LoginPage from 'pages/LoginPage';
 import ContactPage from 'pages/ContactPage';
+import { refreshUser } from 'redux/auth/auth-operations';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { RestrictedRoute } from './RestrictedRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
   return (
     <div
       style={{
@@ -20,9 +29,24 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/contacts" element={<ContactPage />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute component={Register} redirectTo="/contacts" />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute component={LoginPage} redirectTo="/contacts" />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute component={ContactPage} redirectTo="/login" />
+            }
+          />
         </Route>
       </Routes>
     </div>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,13 @@
 import { NavLink } from 'react-router-dom';
+import { getIsLoggedIn } from 'redux/auth/auth-selectors';
+import { useSelector } from 'react-redux';
 
 export const Navigation = () => {
+  const isLoggedIn = useSelector(getIsLoggedIn);
   return (
     <nav>
       <NavLink to="/">Home</NavLink>
-
-      <NavLink to="/contacts">Contacts</NavLink>
+      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
     </nav>
   );
 };
