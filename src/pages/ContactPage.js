c721eb02bdac312fd72f87f5f7e401180486aa94
Natalia <nataliamaliovana.nm@gmail.com>
2023-10-16 15:56:26 +0700

    message

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,15 @@
 import Form from 'components/Form/Form';
 import Filter from 'components/Filter/Filter';
 import Contacts from 'components/Contacts/Contacts';
+import { fetchContacts } from 'redux/contacts/contacts-operations';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
 export default function ContactPage() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <div>
       <h1>Phonebook</h1>
